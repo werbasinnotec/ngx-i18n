acfb86542f8d5457ff2879c05db47089e0a4b816
mwiesmueller <martin.wiesmueller@werbas-ag.com>
2017-09-25 10:45:31 +0200

    Expand function parser with test. Now it's possible to fet getTranslation calls, too

--- test/cli/parsers/func.parserTest.js
+++ test/cli/parsers/func.parserTest.js
@@ -25,4 +25,14 @@ describe('funcparser....', () => {
     assert.that(res[0]).is.equalTo('foobar test');
     done();
   });
+
+  it('... returns an array when multiple function calls are present', (done) => {
+    const code = "this.i18n.translate('foobar test'); this.otherFunction.getTranslation('Hello World');";
+    const res = funcparser(code);
+
+    assert.that(res.length).is.equalTo(2);
+    assert.that(res[0]).is.equalTo('foobar test');
+    assert.that(res[1]).is.equalTo('Hello World');
+    done();
+  });
 });
