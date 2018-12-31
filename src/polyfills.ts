86595b26e03097594eb702b0794fc1e01286f331
Martin Wiesmüller <martin.wiesmueller@werbas-ag.com>
2018-12-31 09:53:01 +0100

    Remove HammerJS and WEB-Anminations. Is not necessary for this module

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -73,8 +73,6 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'hammerjs';
-import 'web-animations-js';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
